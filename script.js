3ee3e0ce88aacb38cfed765ce69a85991ed9785c
Joel <joefirebat@hotmail.com>
2025-09-10 23:50:58 -0300

    JS Finalizado autoPlay

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 const videos = document.querySelectorAll('video')
-const container = document.querySelector('videos-container')
+const container = document.querySelector('.videos-container')
 
+videos.forEach(v => {
+    v.muted = true
+    v.playsInline = true
+})
+function tocarVideo() {
 
-function tocarVideo(){
+    const alturaTela = container.clientHeight
+    const videoAtual = Math.round(container.scrollTop / alturaTela)
 
-  const alturaTela =  window.innerHeight
+    videos.forEach((video, index) => {
+        if (index === videoAtual) {
+            video.play().catch(() => { })
+        } else {
+            video.pause()
+            video.currentTime = 0
+        }
+    })
 
-  const videoAtual = Math.round(container.scrollTop / alturaTela)
-
-  console.log(videoAtual)
-
-  videos.forEach
 }
+
+// no clique libera o som do vídeo atual
+container.addEventListener('click', () => {
+    const alturaTela = container.clientHeight
+    const videoAtual = Math.round(container.scrollTop / alturaTela)
+    const video = videos[videoAtual]
+    if (video){
+         video.muted = false
+         video.volume = 1
+         video.play().catch(()=>{})
+    }
+} {once: true})
+
+container.addEventListener('scroll', tocarVideo)
+tocarVideo()
+
+
